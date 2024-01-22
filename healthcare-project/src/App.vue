796b7f7643c915c6788a4748a94007c0a4d6f6df
vivre327 <hhaayy27@naver.com>
2024-01-22 23:12:01 +0900

    01 초기 업로ㅡ

---
+++ healthcare-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <IntroAni/>
+</template>
+
+<script>
+import IntroAni from './components/IntroAni.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IntroAni
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ healthcare-project/src/components/IntroAni.vue
@@ -0,0 +1,36 @@
+<template>
+<div>hi</div>
+ <div id="lottie-container"></div>
+</template>
+
+<script>
+export default {
+  name: 'IntroAni',
+  mounted() {
+    try {
+      window.lottie.loadAnimation({
+        container: document.getElementById('lottie-container'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: require('@/assets/sample.json')
+      });
+    } catch (error) {
+      console.error('Lottie 로딩 중 오류 발생:', error);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+#lottie-container {
+  width: 500px;
+  height: 500px;
+  background-color: #ffff00;
+}
+
+</style>
